f10adf1cdba44126c37bfdfd9a5de1220147adee
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-21 00:52:59 -0500

    Create animals table.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id              INT GENERATED ALWAYS AS IDENTITY,
+    name            VARCHAR(150),
+    date_of_birth   DATE,
+    escape_attempts INT,
+    neutered        BOOLEAN,
+    weight_kg       DECIMAL(5,2),
+    PRIMARY KEY(id)
 );
