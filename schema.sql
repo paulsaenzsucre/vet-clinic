4ddfc7d426eac5e3b09ba6ce661d0681f41601db
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-23 11:33:31 -0500

    Fix typo.

--- schema.sql
+++ schema.sql
@@ -50,6 +50,6 @@ CREATE TABLE specializations (
 CREATE TABLE visits (
   animal_id     INT REFERENCES animals(id),
   vet_id        INT REFERENCES vets(id),
-  date_of_visit DATE
+  date_of_visit DATE,
   PRIMARY KEY(animal_id, vet_id)
 );
