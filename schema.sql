47121fc116a7c3e636f4e9d88064024aeb289d14
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-23 11:21:37 -0500

    Add date of visit field to visits table.

--- schema.sql
+++ schema.sql
@@ -34,21 +34,22 @@ ADD COLUMN species_id INT REFERENCES species(id),
 ADD COLUMN owner_id INT REFERENCES owners(id);
 
 CREATE TABLE vets (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(150),
-  age INT,
-  date_of_graduation DATE,
+  id                  INT GENERATED ALWAYS AS IDENTITY,
+  name                VARCHAR(150),
+  age                 INT,
+  date_of_graduation  DATE,
   PRIMARY KEY(id)
 );
 
 CREATE TABLE specializations (
-  vet_id INT REFERENCES vets(id),
-  specie_id INT REFERENCES species(id),
+  vet_id        INT REFERENCES vets(id),
+  specie_id     INT REFERENCES species(id),
   PRIMARY KEY(vet_id, specie_id)
 );
 
 CREATE TABLE visits (
-  animal_id INT REFERENCES animals(id),
-  vet_id INT REFERENCES vets(id),
+  animal_id     INT REFERENCES animals(id),
+  vet_id        INT REFERENCES vets(id),
+  date_of_visit DATE
   PRIMARY KEY(animal_id, vet_id)
 );
