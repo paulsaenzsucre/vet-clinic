2ba961185ea22d25bc32ff425234ffa9bd27378b
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-28 14:45:48 -0500

    Create medical_histories_treatments, invoices & invoice_items tables.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,26 @@ CREATE TABLE treatments(
         name VARCHAR(100)
 );
 
+CREATE TABLE medical_histories_treatments (
+        medical_histories_id INT REFERENCES medical_histories(id),
+        treatments INT REFERENCES treatments(id)
+);
+ 
+CREATE TABLE invoices (
+        id SERIAL NOT NULL PRIMARY KEY,
+        total_amount DECIMAL,
+        generated_at TIMESTAMP,
+        payed_at TIMESTAMP,
+        medical_history_id INT NOT NULL,
+        UNIQUE(medical_history_id),
+        FOREIGN KEY(medical_history_id) references medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+	id SERIAL NOT NULL PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT REFERENCES invoices(id),
+	treatment_id INT REFERENCES treatments(id)
+);
